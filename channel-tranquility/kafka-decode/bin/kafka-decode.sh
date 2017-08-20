70e3102a6eeedef7ac7c6a4629f26ed9286c7909
moran <760047616@qq.com>
2017-08-20 22:37:55 +0800

    提交測試

--- channel-tranquility/kafka-decode/bin/kafka-decode.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-java -Dlogback.configurationFile=logback.xml -cp ./lib/*:./channel-tranquility-kafka-decode-1.0-SNAPSHOT.jar \
-    com.metamx.tranquility.kafka.KafkaMain -f rdq.json
