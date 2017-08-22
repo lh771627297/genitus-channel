e6ee288a054475f9e9f8bbea7de7141ffdc3afd4
moran <760047616@qq.com>
2017-08-22 18:57:08 +0800

    新建channel-tracker

--- channel-tracker/src/main/scala/com/moran/genitus/channel/tracker/AppBoot.scala
+++ channel-tracker/src/main/scala/com/moran/genitus/channel/tracker/AppBoot.scala
@@ -1,6 +1,8 @@
 package com.moran.genitus.channel.tracker
 
-import com.moran.genitus.channel.tracker.module.RestModule
+import com.moran.genitus.channel.tracker.config.EsConf
+import com.moran.genitus.channel.tracker.module.{EsModule, RestModule}
+import com.moran.genitus.channel.tracker.service.EsService
 import org.genitus.cardiac.http.{HttpConf, HttpModule, HttpService}
 import org.genitus.cardiac.metrics.MetricsModule
 import org.rogach.scallop.ScallopConf
@@ -12,14 +14,15 @@ import org.rogach.scallop.ScallopConf
 object AppBoot extends org.genitus.cardiac.App{
 
   //收集相关参数设置
-  lazy val conf = new ScallopConf(args) with HttpConf
+  lazy val conf = new ScallopConf(args) with HttpConf with EsConf
 
   //使用参数配置各个模块
   override def modules() = {
     Seq(
       new HttpModule(conf),
       new MetricsModule,
-      new RestModule
+      new RestModule,
+      new EsModule(conf)
     )
   }
 
@@ -27,4 +30,5 @@ object AppBoot extends org.genitus.cardiac.App{
 
   //启动各项服务
   run(classOf[HttpService])
+  run(classOf[EsService])
 }
